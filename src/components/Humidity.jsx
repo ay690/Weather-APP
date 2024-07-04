2ae7f7070f0e155ebc37e9575bf3f916116167aa
Aniket Yadav <aniketyadav690@gmail.com>
2024-07-05 00:06:32 +0530

    fix: eslint error

--- src/components/Humidity.jsx
+++ src/components/Humidity.jsx
@@ -1,3 +1,5 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable no-unused-vars */
 import thumbup from "../assets/thumbup.png";
 import thumbyo from "../assets/thumbyo.png";
 import thumbdown from "../assets/thumbdown.png";
