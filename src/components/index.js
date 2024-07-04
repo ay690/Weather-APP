11c55ea884594af8f5e1de602704c62b7766a973
Aniket Yadav <aniketyadav690@gmail.com>
2024-07-04 23:51:48 +0530

    feat: wind status card

--- src/components/index.js
+++ src/components/index.js
@@ -3,5 +3,6 @@ import Search from "./Search";
 import Primarydata from "./Primarydata";
 import Secondarydata from "./Secondarydata";
 import UVindex from "./UVindex";
+import WindStatus from "./WindStatus";
 
-export { Main, Search, Primarydata, Secondarydata, UVindex };
+export { Main, Search, Primarydata, Secondarydata, UVindex, WindStatus };
